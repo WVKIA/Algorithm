fb45cb4f4f5920aa74b5ea38a2eb25a7bc4aac4b
wuk <wuk@@mchz.com.cn>
2018-04-23 12:40:10 +0800

    add file

---
+++ src/ArraysModel/q001_TwoSum.java
@@ -0,0 +1,100 @@
+package Arrays;
+
+import javax.xml.transform.Source;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+ Example:
+
+ Given nums = [2, 7, 11, 15], target = 9,
+
+ Because nums[0] + nums[1] = 2 + 7 = 9,
+ return [0, 1].
+ */
+public class q001_TwoSum {
+
+    /**
+     * 暴力穷举法
+     * 循环两次nums，然后得到相加数为target的下标，同时限制下标不能一致
+     * 时间复杂度为O(n^2)
+     * @param arrs
+     * @param target
+     * @return
+     */
+    public static int[] solution_1(int[] arrs, int target) {
+        if (arrs == null || arrs.length == 0) {
+            return null;
+        }
+        int[] result = new int[2];
+        for (int i = 0; i < arrs.length; i++) {
+            int a = arrs[i];
+            for (int j = 0; j < arrs.length; j++) {
+                if (i != j) {
+                    if (a + arrs[j] == target) {
+                        result[0] = i;
+                        result[1] = j;
+                        return result;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 先看时间复杂度 O(n)
+     * 对解法进行分析
+     * 1. 首先将数组的值作为key，下标作为value放到map里，注意如果有重复的value值，那就是最后的为value，因为题目标志有一个唯一解，所以这个没关系，不可能出现两个位置都是目标结果的
+     * 2. 循环数组，判断targget-num[i]是否在map里，如果在，取出下标就是我们需要的，但还要注意取的下标不能和我们循环的下标一致，
+     * 即map.get(target-num[i]) != i，就是防止如果target为8，其中有个4，我们拿到了4的下标又循环了数组，得出4+4=8下标取出来都是一样 的
+     *
+     * @param arrs
+     * @param target
+     * @return
+     */
+    public int[] solution_2(int[] arrs, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arrs.length; i++) {
+            map.put(arrs[i], i);
+        }
+        for (int i = 0; i < arrs.length; i++) {
+            int other = target - arrs[i];
+            if (map.containsKey(other) && map.get(other) != i) {
+                return new int[]{i,map.get(other)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * 将解法2两个循环合并成一个
+     * @param arrs
+     * @param target
+     * @return
+     */
+    public int[] solution_3(int[] arrs, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arrs.length; i++) {
+            if (map.containsKey(target - arrs[i])) {
+                return new int[]{i,map.get(target-arrs[i])};
+            }
+            map.put(arrs[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution_1(nums, target);
+        System.out.println(result.length == 2);
+        System.out.println(result[0] == 0);
+        System.out.println(result[1] == 1);
+
+    }
+}
---
+++ src/ArraysModel/q004_MedianOfTwoSortedArrays.java
@@ -0,0 +1,107 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+ Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+ Example 1:
+ nums1 = [1, 3]
+ nums2 = [2]
+
+ The median is 2.0
+ Example 2:
+ nums1 = [1, 2]
+ nums2 = [3, 4]
+
+ The median is (2 + 3)/2 = 2.5
+ */
+public class q004_MedianOfTwoSortedArrays {
+    /**
+     * 暂时不考虑时间复杂度O(log(m+n))的限制，首先想办法解决问题
+     * 问题是取得两个数组的中位数，第一个考虑的办法是两个数组合并，然后取得中位数
+     *
+     * 第一种，暴力排序，浪费两个数组已经排序的优势
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public static double findMedianSortedArrays_1(int[] nums1, int[] nums2) {
+        List<Integer> list = new ArrayList<>(nums1.length + nums2.length);
+        for (int i : nums1) {
+            list.add(i);
+        }
+        for (int i : nums2) {
+            list.add(i);
+        }
+        Collections.sort(list);
+        int middel = list.size() / 2;
+        double d=0.0;
+        if (list.size() % 2 == 0) {
+            //注意易错点 除以 2.0,否则转型导致结果错误
+            d=(list.get(middel)+list.get(middel-1))/2.0;
+        }else {
+            d = list.get(middel);
+        }
+        return d;
+    }
+
+
+    /**
+     * 同样不关心时间复杂度，利用已经排序的条件进行组合成一个数组
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public static double findMedianSortedArrays_2(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            throw new IllegalArgumentException("Arrays is error");
+        }
+        int count = nums1.length + nums2.length;
+        int[] countarr = new int[count];
+        int index1 = 0;
+        int index2 = 0;
+        int indexcount=0;
+        double d = 0.0;
+        while (index1 < nums1.length && index2 < nums2.length) {
+            if (nums1[index1] <= nums2[index2]) {
+                countarr[indexcount++] = nums1[index1++];
+            }else {
+                countarr[indexcount++] = nums2[index2++];
+            }
+        }
+
+        //如果有一方数组用完
+        while (index1 < nums1.length) {
+            countarr[indexcount++] = nums1[index1++];
+        }
+        while (index2 < nums2.length) {
+            countarr[indexcount++] = nums2[index2++];
+        }
+
+        int middel=count/2;
+        if (count % 2 == 0) {
+            d = (countarr[middel] + countarr[middel - 1]) / 2.0;
+        }else {
+            d = countarr[middel];
+        }
+        return d;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1, 3};
+        int[] nums2 = new int[]{2};
+        System.out.println(findMedianSortedArrays_2(nums1,nums2));
+
+        nums1 = new int[]{1, 2};
+        nums2 = new int[]{3, 4};
+        System.out.println(findMedianSortedArrays_2(nums1,nums2));
+    }
+
+
+}
